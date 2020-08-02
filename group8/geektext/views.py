ee93130e70bc45a5034a08db04ab41c69b4a3188
leopants <leopanta99@gmail.com>
2020-08-02 01:34:13 -0400

    added DjangoRestFramework

--- group8/geektext/views.py
+++ group8/geektext/views.py
@@ -1,48 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Books
+from .serializers import BookSerializer
 import json
 from django.core.exceptions import ObjectDoesNotExist
 from django.core import serializers
 
 
 #Get books and display them
+@api_view(['GET'])
 def index(request):
-    books_json = serializers.serialize("json", Books.objects.all())
-    data = {"all_books": books_json}
-    return JsonResponse(data)
+    books = Books.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
 
+@api_view(['GET'])
 def genre(request):
     requested_genre = json.loads(request.body)["genre"]
     try:
-        books_json = serializers.serialize("json", list(Books.objects.filter(genre=requested_genre)))
-        data = {requested_genre + " books": books_json}
-        return JsonResponse(data)
+        books = Books.objects.filter(genre=requested_genre)
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
     except ObjectDoesNotExist:
-        return HttpResponse("No books matching that genre exist")
+        return Response("No books matching that genre exist")
 
+@api_view(['GET'])
 def bestsellers(request):
     try:
-        books_json = serializers.serialize("json", list(Books.objects.order_by('-number_sold')[:10]))
-        data = {"Bestsellers": books_json}
-        return JsonResponse(data)
+        books = list(Books.objects.order_by('-number_sold')[:10])
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
     except ObjectDoesNotExist:
-        return HttpResponse('No books in the database')
+        return Response('No books in the database')
 
+@api_view(['GET'])
 def rating_sort(request):
     better_than = json.loads(request.body)["rating"] 
     try:
-        books_json = serializers.serialize("json", list(Books.objects.filter(rating__gte=better_than)))
-        data = {str(better_than) + " or better books" : books_json}
-        return JsonResponse(data)
+        books = list(Books.objects.filter(rating__gte=better_than))
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
     except ObjectDoesNotExist:
-        return HttpResponse("No books better than that rating are available")
+        return Response("No books better than that rating are available")
 
+@api_view(['GET'])
 def random_list(request):
     num_books = json.loads(request.body)["amount"]
     try:
-        books_json = serializers.serialize("json", list(Books.objects.order_by('?')[:num_books]))
-        data = {str(num_books) + " random books" : books_json}
-        return JsonResponse(data)
+        books = list(Books.objects.order_by('?')[:num_books])
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
     except ObjectDoesNotExist:
-        return HttpResponse("No books found in the database")
+        return Response("No books found in the database")
